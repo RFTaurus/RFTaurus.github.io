b2c9225a95b4fc7a0be33141b9f1dc41ac9c8ad5
RFTaurus <rakish_fanquer@pahamify.com>
2022-08-07 22:49:14 +0700

    First Initialize Portfolio Page
    First Initialize Portfolio Page

---
+++ precache-manifest.b5e8908afd243a26f3f32c534c3ede96.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce902604b25b416ff096",
+    "url": "/css/app.4ef44153.css"
+  },
+  {
+    "revision": "cd19e5ef1f54be4c26d6caeb9885672c",
+    "url": "/img/assets/codepanda-logo.png"
+  },
+  {
+    "revision": "6441440edb4dd88ff74798a3b85e8edf",
+    "url": "/img/assets/drive-logo.png"
+  },
+  {
+    "revision": "08695dde0be355e4b00928aaec8b03f3",
+    "url": "/img/assets/github-logo.png"
+  },
+  {
+    "revision": "4a7a1e0d26c0b21e3cdd4db42becdd33",
+    "url": "/img/assets/linkedin-logo.png"
+  },
+  {
+    "revision": "6f39029a4910392a2128d24a40f1b842",
+    "url": "/img/assets/my-logo.jpg"
+  },
+  {
+    "revision": "6441440edb4dd88ff74798a3b85e8edf",
+    "url": "/img/drive-logo.6441440e.png"
+  },
+  {
+    "revision": "4a2955abf9abc9d05e754370665f9cef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce902604b25b416ff096",
+    "url": "/js/app.6ccb339a.js"
+  },
+  {
+    "revision": "5aaeeca584d3976624e9",
+    "url": "/js/chunk-vendors.c541bfb3.js"
+  },
+  {
+    "revision": "5618fa7d93b9f029629eb1ad1387bf14",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
